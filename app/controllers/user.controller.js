696c0543220ae259e081e5fbc5a655340e1eccb9
Costantino Dionis Gwaka <gwaka94@gmail.com>
2024-07-13 13:58:18 +0300

    fix

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -59,7 +59,7 @@ exports.findOne = (req, res) => {
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error retrieving Users with id=" + id
+        message: "Error retrieving Users with id= " + id
       });
     });
 };
